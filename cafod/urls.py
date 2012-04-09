3a0a5241cf576dee7e75a4a1075be69608e72320
Carlos Rocha <crocha09.09@gmail.com>
2012-04-09 08:55:01 -0600

    agregando el proyecto cafod

---
+++ cafod/urls.py
@@ -0,0 +1,28 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'cafod.views.home', name='home'),
+    # url(r'^cafod/', include('cafod.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin
+    (r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    (r'^accounts/logout/$', 'notas.views.logout_page'),
+    (r'^$', 'notas.views.index'),
+    #url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'registration/login.html'}, 
+        #name='user-login'),
+    #url(r'^logout/$', 'notas.views.logout_page', name="logout"),
+    url(r'^notas/', include('notas.urls')),
+    url(r'^contrapartes/', include('contrapartes.urls')),
+    url(r'^agendas/', include('agendas.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^tagging_autocomplete/', include('tagging_autocomplete.urls')),
+    url(r'^foro/', include('foros.urls')),
+)

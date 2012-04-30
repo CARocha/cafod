d4e7ede1ee1ae25999319b79538178a069320715
Byron Corrales <byroncorrales@gmail.com>
2012-04-30 09:37:09 -0600

    aportes en foro

---
+++ magicembed/templatetags/magicembed.py
@@ -0,0 +1,20 @@
+from django import template
+from magicembed.providers import get_provider
+register = template.Library()
+
+@register.filter
+def magicembed(value, arg=None):
+    '''value is the url and arg the size tuple
+    ussage: {% http://myurl.com/|magicembed:"640x480" %}'''
+    arg = [int(item) for item in arg.split('x')]
+    provider = get_provider(value, arg)
+    return provider.render_video()
+
+@register.filter
+def magicthumbnail(value):
+    '''value is the url and arg the link_to another url
+    ussage: {% http://myurl.com/|magicthumbnail: '/some/url' %}'''
+    provider = get_provider(value)
+    return provider.render_thumbnail()
+
+magicembed.is_safe = True

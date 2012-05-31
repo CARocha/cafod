f5d2e8cf7a6aadd5597233cdf589e31ad5f997e5
sacrac <crocha09.09@gmail.com>
2012-05-31 14:44:38 -0600

    fix ckeditor in flatpages

--- agendas/admin.py
+++ agendas/admin.py
@@ -2,6 +2,26 @@ from django.contrib import admin
 from models import *
 from ckeditor.widgets import CKEditorWidget
 from agendas.forms import *
+from django.contrib.flatpages.admin import FlatPageAdmin
+from django.contrib.flatpages.models import FlatPage
+
+from django.contrib.flatpages.admin import FlatPageAdmin as FlatPageAdminOld
+from django.contrib.flatpages.admin import FlatpageForm as FlatpageFormOld
+ 
+from django import forms
+from ckeditor.widgets import CKEditorWidget
+ 
+class FlatpageForm(FlatpageFormOld):
+    content = forms.CharField(widget=CKEditorWidget())
+    class Meta:
+        model = FlatPage
+ 
+ 
+class FlatPageAdmin(FlatPageAdminOld):
+    form = FlatpageForm
+ 
+admin.site.unregister(FlatPage)
+admin.site.register(FlatPage, FlatPageAdmin)
 
 class DocumentosInline(generic.GenericTabularInline):
     model = Documentos
